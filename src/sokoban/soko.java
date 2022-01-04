632b76756efe47269d7b7d1ee1e97aaa7a422776
Stefan Jocic <jocicstefan91@gmail.com>
2022-01-04 21:06:44 +0100

    make a move

--- src/sokoban/soko.java
+++ src/sokoban/soko.java
@@ -1,7 +1,8 @@
 package sokoban;
-
+import java.util.Scanner;
 public class soko {
 
+	
 	public static void main(String[] args) {
 		String [] soko = { "U"," "," "," "," "
 						  ," ","B","X"," "," "
@@ -9,9 +10,21 @@ public class soko {
 						  ," "," "," "," "," "
 						  ," "," "," "," ","E"};
 		
+		Scanner s = new Scanner(System.in);
+		
 		System.out.println("  SOKOBAN");
 		print(soko);
 		
+		System.out.print("Odigrajte potez");
+		
+		
+		while(!endGame(soko)) {
+			System.out.print("Odigrajte potez");
+			String move = s.next();
+			
+			makeAMove(move, soko);
+			print(soko);
+		}
 
 	}
 	
@@ -35,12 +48,61 @@ public class soko {
 			
 	}
 	
-	public static boolean endGame(String move, String [] table) {
+	public static boolean endGame(String [] table) {
 		
-		if(table(table.length-1).equals("B")) {
+		if(table[table.length-1].equals("B")) {
 			return true;
 		}
 		return false;
+	}
+	
+	public static void makeAMove(String move, String [] table) {
+		
+		switch (move) {
+		case "a":
+				for (int i = 0; i < table.length; i++) {
+					if(table[i].equals("U")) {
+						table[i]= " ";
+						i--;
+						table[i]= "U";
+					}
+				}
+			break;
+			
+		case "d":
+			for (int i = 0; i < table.length; i++) {
+				if(table[i].equals("U")) {
+					table[i]= " ";
+					i++;
+					table[i]= "U";
+				}
+			}
+		break;
+
+		case "w":
+			for (int i = 0; i < table.length; i++) {
+				if(table[i].equals("U")) {
+					table[i]= " ";
+					i=i-5;
+					table[i]= "U";
+				}
+			}
+		break;
+		
+		case "s":
+			for (int i = 0; i < table.length; i++) {
+				if(table[i].equals("U")) {
+					table[i]= " ";
+					i=i+5;
+					table[i]= "U";
+				}
+			}
+		break;
+		
+		default:
+			System.out.println("Uneli ste nevalidnu vrednost");
+			break;
+		}
 	}
 
 }
